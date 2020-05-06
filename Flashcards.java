58680f95eca4c0722a544bcfb83a5e2d8871df81
Nicole <62617297+rolliecoley@users.noreply.github.com>
2020-05-06 16:16:39 -0400

    Create Flashcards.java

---
+++ Flashcards.java
@@ -0,0 +1,26 @@
+public class Flashcards {
+    private String word;
+    private String definition;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    public Flashcards(){
+
+
+         }
+
+}
